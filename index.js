63aa076ad564d44deff38913311e4ae7552a7d75
Ramiro <ramiroruiz578@gmail.com>
2017-11-20 22:23:41 +0100

    test

--- index.js
+++ index.js
@@ -40,10 +40,10 @@ const port = process.env.PORT || 3000
 
 // app.use(express.static(__dirname + '/dist/'));
 
-app.use(express.static(path.join(process.cwd(), 'client/dist/')))
+app.use(express.static(path.join(process.cwd(), '/client')))
 
 app.get('/*', (req, res) => {
-    res.sendfile(path.join('client/dist/'))
+    res.sendfile(path.join('/client/'))
 })
 app.use('/api', router.app)
 app.use('/api', auth.app)
