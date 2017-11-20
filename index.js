a86eb4758f9c623b89b6bbf6020d033ab4022a13
Ramiro6 <fresco_esp@hotmail.com>
2017-11-20 21:38:40 +0100

    test

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ const port = process.env.PORT || 3000
     })
 // }
 
-app.use(express.static(__dirname + '/dist/'));
+// app.use(express.static(__dirname + '/dist/'));
 
-// app.use(express.static(path.join(process.cwd(), 'dist')))
+app.use(express.static(path.join(process.cwd(), '/dist/')))
 
 app.use('/api', router.app)
 app.use('/api', auth.app)
